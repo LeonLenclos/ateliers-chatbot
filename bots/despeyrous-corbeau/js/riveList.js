61c76536b49d9af8ef3b0e3ef15c9f8b872a029c
Léon Lenclos <leon.lenclos@gmail.com>
2020-02-05 22:05:47 +0100

    j2 despeyrous

--- bots/despeyrous-corbeau/js/riveList.js
+++ bots/despeyrous-corbeau/js/riveList.js
@@ -1,4 +1,8 @@
 // This array must contain the list of all the rive to include.
 const riveList = [
 "brain/special.rive",
+"brain/elisa-stella.rive",
+"brain/elouane-jason.rive",
+"brain/enzo-heiva.rive",
+"brain/flo.rive",
 ]
--- bots/despeyrous-stitch/js/riveList.js
+++ bots/despeyrous-stitch/js/riveList.js
@@ -1,4 +1,11 @@
 // This array must contain the list of all the rive to include.
 const riveList = [
 "brain/special.rive",
+"brain/elisa-g.rive",
+"brain/loic-elias.rive",
+"brain/maelle-kataline.rive",
+"brain/nbm.rive",
+"brain/sarah-giovana.rive",
+"brain/timoty-elie.rive",
+"brain/valentine-lucie.rive",
 ]
